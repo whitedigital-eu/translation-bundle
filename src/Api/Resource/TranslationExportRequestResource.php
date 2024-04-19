5c91a234d7f430ab210987de6d6a24cadef607d3
raraworks <rasmanis.raitis@gmail.com>
2024-04-19 13:09:16 +0300

    Adding initial integration into bundle

--- src/Api/Resource/TranslationExportRequestResource.php
+++ src/Api/Resource/TranslationExportRequestResource.php
@@ -1,15 +1,35 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace WhiteDigital\Translation\Api\Resource;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Response;
+use ArrayObject;
 use WhiteDigital\Translation\DataProcessor\TranslationExportRequestDataProcessor;
 
 #[
     ApiResource(
         shortName: 'TranslationExportRequest',
-        operations: [new Post()],
+        operations: [
+            new Post(
+                openapi: new Operation(
+                    responses: [
+                        '201' => new Response(
+                            description: 'Exported translations xlsx file',
+                            content: new ArrayObject([
+                                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' => [
+                                    'schema' => [
+                                        'type' => 'string',
+                                        'format' => 'binary',
+                                    ],
+                                ],
+                            ]),
+                        ),
+                    ],
+                )),
+        ],
         processor: TranslationExportRequestDataProcessor::class,
     ),
 ]
