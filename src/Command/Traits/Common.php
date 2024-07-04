509cab23567546136f44d3df68b16590df8452c7
Dāvis Zālītis <davis.zalitis@whitedigital.eu>
2024-07-04 14:43:57 +0300

    feat: split import/override commands

---
+++ src/Command/Traits/Common.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types = 1);
+
+namespace WhiteDigital\Translation\Command\Traits;
+
+use RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Finder\Finder;
+use WhiteDigital\SiteTree\Entity\SiteTree;
+
+use function array_combine;
+use function array_map;
+use function class_exists;
+use function count;
+use function explode;
+use function getcwd;
+use function unlink;
+
+trait Common
+{
+    protected function configureCommon(bool $required = true): void
+    {
+        if (class_exists(SiteTree::class)) {
+            $trees = $this->em->getRepository(SiteTree::class)->findBy(['parent' => null, 'isActive' => true]);
+            foreach (array_map(static fn (SiteTree $tree) => $tree->getSlug(), $trees) as $locale) {
+                $this->addOption($locale, null, $required ? InputOption::VALUE_REQUIRED : InputOption::VALUE_OPTIONAL, 'File path for ' . $locale . ' translations', null);
+            }
+        } else {
+            $this->addOption('locales', null, $required ? InputOption::VALUE_REQUIRED : InputOption::VALUE_OPTIONAL, 'Required locales, comma separated');
+            $this->addOption('files', null, $required ? InputOption::VALUE_REQUIRED : InputOption::VALUE_OPTIONAL, 'Required files in order of locales argument, comma separated');
+        }
+    }
+
+    protected function getLocalesAndPaths(InputInterface $input, bool $required = true): array
+    {
+        $locales = $paths = [];
+        if (class_exists(SiteTree::class)) {
+            $trees = $this->em->getRepository(SiteTree::class)->findBy(['parent' => null, 'isActive' => true]);
+            foreach (array_map(static fn (SiteTree $tree) => $tree->getSlug(), $trees) as $locale) {
+                $locales[] = $locale;
+                $paths[$locale] = ($file = $input->getOption($locale));
+                if ((null === $file) && $required) {
+                    throw new RuntimeException();
+                }
+            }
+            if ($required && count($locales) !== count($trees)) {
+                throw new RuntimeException();
+            }
+        } else {
+            $locales = explode(',', $input->getOption('locales') ?? '');
+            $paths = explode(',', $input->getOption('files') ?? '');
+            if (count($locales) !== count($paths)) {
+                throw new RuntimeException();
+            }
+
+            $paths = array_combine($locales, $paths);
+        }
+
+        return [
+            'locales' => $locales,
+            'paths' => $paths,
+        ];
+    }
+
+    protected function cleanup(): void
+    {
+        $finder = new Finder();
+        $finder->files()->in(getcwd() . '/translations')->name('*.json');
+        foreach ($finder as $file) {
+            $filePath = $file->getRealPath();
+            @unlink($filePath);
+        }
+    }
+}
--- src/Command/TransUnitImportCommand.php
+++ src/Command/TransUnitImportCommand.php
@@ -2,37 +2,43 @@
 
 namespace WhiteDigital\Translation\Command;
 
+use Doctrine\ORM\EntityManagerInterface;
 use JsonException;
+use Lexik\Bundle\TranslationBundle\Entity\TransUnit;
 use RuntimeException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\ExceptionInterface;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\Finder\Finder;
 use WhiteDigital\EntityResourceMapper\EntityResourceMapperBundle;
 
+use function array_key_exists;
+use function array_keys;
+use function array_map;
+use function array_shift;
+use function basename;
 use function explode;
 use function file_get_contents;
 use function file_put_contents;
 use function getcwd;
-use function implode;
+use function gettype;
+use function is_array;
 use function is_dir;
-use function is_file;
+use function iterator_to_array;
 use function json_decode;
 use function json_encode;
+use function json_last_error;
 use function mkdir;
 use function rename;
 use function sprintf;
 use function str_replace;
-use function unlink;
 
 use const DIRECTORY_SEPARATOR;
+use const JSON_ERROR_NONE;
 use const JSON_THROW_ON_ERROR;
 
 #[AsCommand(
@@ -41,27 +47,37 @@ use const JSON_THROW_ON_ERROR;
 )]
 class TransUnitImportCommand extends Command
 {
+    use Traits\Common;
+
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {
+        parent::__construct();
+    }
+
     /**
      * @throws ExceptionInterface
      * @throws JsonException
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $locale = $input->getArgument('locale');
-        $path = $input->getArgument('path');
+        ['locales' => $locales, 'paths' => $paths] = $this->getLocalesAndPaths($input);
 
-        $domains = [];
-        if (null !== $path) {
-            if (!is_file($filename = getcwd() . '/' . $path)) {
-                throw new FileNotFoundException(sprintf('File %s does not exist', $filename));
-            }
+        if (!$this->jsonStructureCompare($paths)) {
+            throw new RuntimeException();
+        }
 
-            $newPath = getcwd() . DIRECTORY_SEPARATOR . 'translations';
+        $newPath = getcwd() . DIRECTORY_SEPARATOR . 'translations';
 
-            if (!is_dir($newPath) && !mkdir($newPath, recursive: true) && !is_dir($newPath)) {
-                throw new RuntimeException(sprintf('Directory "%s" was not created', $newPath));
-            }
+        if (!is_dir($newPath) && !mkdir($newPath, recursive: true) && !is_dir($newPath)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not created', $newPath));
+        }
 
+        $bufferedOutput = new BufferedOutput();
+        $extract = $this->getApplication()?->find('translation:extract');
+
+        foreach ($paths as $locale => $path) {
+            $filename = getcwd() . '/' . $path;
             $json = json_decode(file_get_contents($filename), true, 512, JSON_THROW_ON_ERROR);
             $translations = EntityResourceMapperBundle::makeOneDimension($json, onlyLast: true);
 
@@ -71,48 +87,52 @@ class TransUnitImportCommand extends Command
                 $content[$domain][$k] = $value;
             }
 
-            $paths = [];
             foreach ($content as $domain => $translations) {
-                $paths[] = $filePath = $newPath . DIRECTORY_SEPARATOR . $domain . '.' . $locale . '.json';
+                $filePath = $newPath . DIRECTORY_SEPARATOR . $domain . '.' . $locale . '.json';
                 file_put_contents($filePath, json_encode($translations, JSON_THROW_ON_ERROR));
-                $domains[] = $domain;
+            }
+
+            $arrayInput = new ArrayInput([
+                '--force' => true,
+                '--format' => 'json',
+                'locale' => $locale,
+            ]);
+
+            $extract->run($arrayInput, $bufferedOutput);
+
+            $output->write($bufferedOutput->fetch());
+
+            $finder = new Finder();
+            $finder->files()->in(getcwd() . '/translations')->name('*+intl-icu.' . $locale . '.json');
+            foreach ($finder as $file) {
+                @rename($file->getRealPath(), str_replace('+intl-icu', '', $file->getRealPath()));
             }
         }
 
-        $bufferedOutput = new BufferedOutput();
+        $data = [];
+        foreach ($locales as $locale) {
+            $finder = new Finder();
+            $finder->files()->in(getcwd() . '/translations')->name('*.' . $locale . '.json');
 
-        $extract = $this->getApplication()?->find('translation:extract');
-        $arrayInput = new ArrayInput([
-            '--force' => true,
-            '--format' => 'json',
-            'locale' => $locale,
-        ]);
+            $data[$locale] = array_keys(EntityResourceMapperBundle::makeOneDimension($this->readJsonFiles(array_keys(iterator_to_array($finder->files())), $locale), onlyLast: true));
+        }
 
-        $extract->run($arrayInput, $bufferedOutput);
-
-        $output->write($bufferedOutput->fetch());
-
-        $finder = new Finder();
-        $finder->files()->in(getcwd() . '/translations')->name('*+intl-icu.' . $locale . '.json');
-        foreach ($finder as $file) {
-            @rename($file->getRealPath(), str_replace('+intl-icu', '', $file->getRealPath()));
+        $found = [];
+        foreach ($data as $translations) {
+            foreach ($translations as $translation) {
+                [$domain, $key] = explode('.', $translation, 2);
+                if (!isset($found[$domain][$key])) {
+                    $found[$domain][$key] = 1;
+                }
+            }
         }
 
         $command = $this->getApplication()?->find('lexik:translations:import');
         $arguments = [
             '--no-interaction' => true,
             '--cache-clear' => true,
-            '--locales' => [$locale],
+            '--locales' => $locales,
         ];
-
-        if ($input->getOption('override') ?? false) {
-            if ([] === $domains) {
-                throw new RuntimeException('Override only works with input file');
-            }
-
-            $arguments['--force'] = true;
-            $arguments['--domains'] = implode(',', $domains);
-        }
 
         $arrayInput = new ArrayInput($arguments);
 
@@ -120,27 +140,98 @@ class TransUnitImportCommand extends Command
 
         $output->write($bufferedOutput->fetch());
 
-        if (null !== $path) {
-            foreach ($paths as $filePath) {
-                unset($filePath);
-            }
+        $this->cleanup();
+
+        foreach ($found as $domain => $keys) {
+            $qb = $this->em->createQueryBuilder();
+            $qb->update(TransUnit::class, 't')
+                ->set('t.isDeleted', ':isDeleted')
+                ->setParameter('isDeleted', false)
+                ->where('t.domain = :domain')
+                ->setParameter('domain', $domain)
+                ->andWhere($qb->expr()->in('t.key', array_keys($keys)))
+                ->getQuery()
+                ->execute();
+
+            $qb = $this->em->createQueryBuilder();
+            $qb->update(TransUnit::class, 't')
+                ->set('t.isDeleted', ':isDeleted')
+                ->setParameter('isDeleted', true)
+                ->where('t.domain = :domain')
+                ->setParameter('domain', $domain)
+                ->andWhere($qb->expr()->notIn('t.key', array_keys($keys)))
+                ->getQuery()
+                ->execute();
         }
 
-        $finder = new Finder();
-        $finder->files()->in(getcwd() . '/translations')->name('*.json');
-        foreach ($finder as $file) {
-            $filePath = $file->getRealPath();
-            @unlink($filePath);
-        }
+        $qb = $this->em->createQueryBuilder();
+        $qb->update(TransUnit::class, 't')
+            ->set('t.isDeleted', ':isDeleted')
+            ->setParameter('isDeleted', true)
+            ->andWhere($qb->expr()->notIn('t.domain', array_keys($found)))
+            ->getQuery()
+            ->execute();
 
         return Command::SUCCESS;
     }
 
     protected function configure(): void
     {
-        $this
-            ->addArgument('locale', InputArgument::REQUIRED, 'Locale')
-            ->addArgument('path', InputArgument::OPTIONAL, 'Path to import file')
-            ->addOption('override', 'o', InputOption::VALUE_NONE, 'Override existing values');
+        $this->configureCommon();
+    }
+
+    private function jsonStructureCompare(array $jsonFiles): bool
+    {
+        $arrays = array_map(static fn (string $json) => json_decode(file_get_contents($json), true), $jsonFiles);
+
+        $baseStructure = array_shift($arrays);
+
+        foreach ($arrays as $array) {
+            if (!$this->compareStructure($baseStructure, $array)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function compareStructure(mixed $array1, mixed $array2): bool
+    {
+        if (gettype($array1) !== gettype($array2)) {
+            return false;
+        }
+
+        if (is_array($array1)) {
+            foreach ($array1 as $key => $value) {
+                if (!array_key_exists($key, $array2) || !$this->compareStructure($value, $array2[$key])) {
+                    return false;
+                }
+            }
+            foreach ($array2 as $key => $value) {
+                if (!array_key_exists($key, $array1)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private function readJsonFiles(array $jsonFiles, string $locale): array
+    {
+        $result = [];
+
+        foreach ($jsonFiles as $jsonFile) {
+            $key = basename($jsonFile, '.' . $locale . '.json');
+            $data = json_decode(file_get_contents($jsonFile), true);
+
+            if (JSON_ERROR_NONE !== json_last_error()) {
+                continue;
+            }
+
+            $result[$key] = $data;
+        }
+
+        return $result;
     }
 }
